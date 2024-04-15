01bf0c2d8a433fdeaae6187ede8497f44ea9f255
Andry Ravelomanantsoa <andryravelomanantsoa8@gmail.com>
2024-04-15 09:07:28 -0400

    form validation

--- src/components/GameForm.vue
+++ src/components/GameForm.vue
@@ -4,39 +4,34 @@ import { useToast } from 'vue-toast-notification'
 import { onMounted, ref } from 'vue'
 import { gameService } from '../services/gameService'
 import type Ship from '../scripts/ship'
-import type Player from '../scripts/player' 
 import router from '@/router'
 
 
+const playerName = ref('');
+const shipName = ref('');
 const listOfShips = ref<Ship[]>();
-const player = ref<Player>();
 const isLoading = ref(false)
 
 //onMounted est utilisée pour exécuter du code spécifiquement après que le composant a été monté dans le DOM (Document Object Model).
 onMounted(async () => {
   isLoading.value = true
-
   // Cherche la liste de tous les vaisseaux spatiaux.
   await gameService.fetchShips().then(input => {listOfShips.value = input })
+  .catch(err => useToast().error( `Erreur avec le service: ${(err as Error).message}. Est-ce que vous avez démarré le backend localement ?`,{ duration: 6000 })) 
+  .finally( () => isLoading.value = false)
 
-  .catch(err => useToast().error( `Erreur avec le service: ${(err as Error).message}. Est-ce que vous avez démarré le backend localement ?`,
-                                  { duration: 6000 }
-  )) 
-  .finally( () =>
-    isLoading.value = false
-  )
+  shipName.value = listOfShips.value![0].name;
 })
 
 function handleFormSubmission() {
-  const playerNameInput : HTMLInputElement | null = document.getElementById('playerName') as HTMLInputElement
-  const playerShip : HTMLOptionElement | null = document.querySelector('option:checked') as HTMLOptionElement
-  if(playerNameInput.value &&  playerShip.value) {
-     router.push({name: 'Mission', params: { playerName: playerNameInput.value, shipName: playerShip.value}})
+  if(playerName.value &&  shipName.value) {
+     router.push({name: 'Mission', params: { playerName: playerName.value , shipName: shipName.value}})
+  }
+  else {
+      useToast().warning(`Veuillez remplir les champs du formulaire`)
   }
 }
-
 </script>
-
 <template>
   <div class="row d-flex justify-content-center align-items-center">
     <div class="col-sm-6">
@@ -44,12 +39,12 @@ function handleFormSubmission() {
         <div class="card-body p-4 p-lg-5">
           <div class="form-outline mb-4">
             <label class="form-label" for="playerName">Votre nom:</label>
-            <input type="text" id="playerName" class="form-control form-control-lg" />
+            <input v-model="playerName" type="text" id="playerName" class="form-control form-control-lg" />
           </div>
           <div class="form-outline mb-4">
             <label class="form-label card-tex" for="shipSelection">Votre vaisseau:</label>
-            <select class="form-select" id="shipSelection" aria-label="Ship selection" >
-              <option v-for="ship in listOfShips" :value="ship.name" :selected="ship.id === 0">{{ ship.name }}</option>
+            <select v-model="shipName" class="form-select" id="shipSelection" aria-label="Ship selection" >
+              <option v-for="ship in listOfShips" :value="ship.name">{{ ship.name }}</option>
             </select>
           </div>
           <button class="btn btn-primary" @click="handleFormSubmission">Débuter la partie</button>
